647604a0620596f83a32272c2447a50fcf3e0aba
nnmkhang <khangnguyen@microsoft.com>
2023-09-11 13:37:50 -0700

    WIP: trying to get hashing states to work

--- src/hash.rs
+++ src/hash.rs
@@ -1,25 +1,97 @@
-use rustls::sign;
-use rustls::crypto;
-use rust_symcrypt::hash::sha256;
-pub struct SHA256;
+use rustls::crypto::hash; // import the hash module from rustls
+use rust_symcrypt::hash::{Sha256State, HashState, HashAlgorithms};
+use rust_symcrypt::hash::Hash as SymCryptHash;
 
+pub struct SHA256; // create a new struct that encapsulates the hash trait
+struct SHA256Context(Sha256State); // creating a "SHA256Context" which is a wrapper over Sha256State
 
-impl hash::Hash for SHA256 {
+pub(crate) struct Hash(dyn SymCryptHash<Result = &'static [u8], State = dyn HashState<Result = [u8]>>);
+
+impl hash::Hash for Hash {
     fn algorithm(&self) -> hash::HashAlgorithm {
-        hash::HashAlgorithm::SHA256
+        self.get_algorithm()
     }
 
     fn output_len(&self) -> usize {
-        32
+        self.get_output_length()
     }
 
-    fn start(&self) -> Box<dyn hash::Context> { // Context is the name for "state" 
-
+    fn start(&self) -> Box<dyn hash::Context> { 
+        Box::new(SHA256Context(Sha256State::new()))
     }
 
-    fn compute(&self, data: &[u8]) -> hash::Output {
-        let result = sha256(data);
-        hash::Output::new(&sha256(data)[..])
+    fn hash(&self, data: &[u8]) -> hash::Output {
+        hash::Output::new(self.hash(data)[..])
     }
-
 }
+
+impl hash::Context for SHA256Context {
+    fn fork_finish(&self) -> hash::Output {
+        let new_context = self.0.copy();
+        hash::Output::new(&new_context.result()[..])
+    }
+
+    fn fork(&self) -> Box<dyn hash::Context> {
+        Box::new(SHA256Context(self.0.copy()))
+    }
+
+    fn finish(self: Box<Self>) -> hash::Output {
+        hash::Output::new(&self.0.result()[..])
+    }
+
+    fn update(&mut self, data: &[u8]) {
+        self.0.append(&data);
+    }
+}
+
+
+fn map_algorithm(symcrypt_algo: rust_symcrypt::hash::HashAlgorithms) -> hash::HashAlgorithm {
+    match symcrypt_algo {
+        rust_symcrypt::hash::SHA256 => hash::HashAlgorithm::SHA256,
+        rust_symcrypt::hash::SHA384 => hash::HashAlgorithm::SHA384,
+    }
+}
+
+// Bellow was how I was doing this before, I would have to impl hash::Hash and hash::Context for both Sha256
+// and Sha384, I wanted to make a trait on the rust-symcrypt side so I would only have to do one impl
+
+
+// pub struct SHA384; // Define the SHA384 struct
+// struct SHA384Context(Sha384State); // creating a "SHA256Context" which is a wrapper over Sha256State
+
+// impl hash::Hash for SHA384 {
+//     fn algorithm(&self) -> hash::HashAlgorithm {
+//         hash::HashAlgorithm::SHA384
+//     }
+
+//     fn output_len(&self) -> usize {
+//         48 // Adjust the output length for SHA-384
+//     }
+
+//     fn start(&self) -> Box<dyn hash::Context> {
+//         Box::new(SHA384Context(Sha384State::new())) // Create a context for SHA-384
+//     }
+
+//     fn hash(&self, data: &[u8]) -> hash::Output {
+//         hash::Output::new(&sha384(data)[..])
+//     }
+// }
+
+// impl hash::Context for SHA384Context {
+//     fn fork_finish(&self) -> hash::Output {
+//         let new_context = self.0.copy();
+//         hash::Output::new(&new_context.result()[..])
+//     }
+
+//     fn fork(&self) -> Box<dyn hash::Context> {
+//         Box::new(SHA384Context(self.0.copy()))
+//     }
+
+//     fn finish(self: Box<Self>) -> hash::Output {
+//         hash::Output::new(&self.0.result()[..])
+//     }
+
+//     fn update(&mut self, data: &[u8]) {
+//         self.0.append(&data);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 //use rust_symcrypt;
+use rustls_symcrypt::hash::*;
+use rustls::crypto::hash::Hash;
 
 fn main() {
-    println!("Hello, world!");
+    // let sha256_instance = SHA256;
+    // let res = sha256_instance.algorithm();
+    // println!("{:?}", res);
+
+    // let len = sha256_instance.output_len();
+    // println!("{:?}", len);
+
     // let data = hex::decode("641ec2cf711e").unwrap();
     // let expected: &str = "cfdbd6c9acf9842ce04e8e6a0421838f858559cf22d2ea8a38bd07d5e4692233";
 
 
-    // let result = hash::SHA256::compute();
-}
-// #[test]
-// fn test_stateless_sha256_hash() {
-//     let data = hex::decode("641ec2cf711e").unwrap();
-//     let expected: &str = "cfdbd6c9acf9842ce04e8e6a0421838f858559cf22d2ea8a38bd07d5e4692233";
+    // let result = sha256_instance.hash(&data); // this also works, instead of what i had below.
+    // //println!("{:?}", result);
+    // assert_eq!(hex::encode(result), expected);
 
-//     let result = sha256(&data);
-//     assert_eq!(hex::encode(result), expected);
-// }
+
+
+    // let new_instance = SHA256::start(&SHA256); // this is creating a new sha256 context or sha 256 "state"
+
+    println!("Hello, world!");
+
+}
